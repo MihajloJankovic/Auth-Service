62859246aa2be2f05c35591d9eb1bee0508b889a
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2023-12-17 11:39:04 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"context"
+	protosava "github.com/MihajloJankovic/Aviability-Service/protos/main"
+	protosAcc "github.com/MihajloJankovic/accommodation-service/protos/main"
+	protosprof "github.com/MihajloJankovic/profile-service/protos/main"
+	protosRes "github.com/MihajloJankovic/reservation-service/protos/genfiles"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net"
 	"os"
@@ -34,9 +39,54 @@ func main() {
 
 	// NoSQL: Checking if the connection was established
 	authRepo.Ping()
+//acc,profile,ava,res
+	conn, err := grpc.Dial("profile-service:9091", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
 
+		}
+	}(conn)
+	cc := protosprof.NewProfileClient(conn)
+	connAva, err := grpc.Dial("avaibility-service:9095", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}(connAva)
+
+	ccava := protosava.NewAccommodationAviabilityClient(connAva)
+	connRes, err := grpc.Dial("reservation-service:9096", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer func(connRes *grpc.ClientConn) {
+		err := connRes.Close()
+		if err != nil {
+
+		}
+	}(connRes)
+	resc := protosRes.NewReservationClient(connRes)
+	connAcc, err := grpc.Dial("accommodation-service:9093", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer func(connAcc *grpc.ClientConn) {
+		err := connAcc.Close()
+		if err != nil {
+
+		}
+	}(connAcc)
+	acc := protosAcc.NewAccommodationClient(connAcc)
 	//Initialize the handler and inject said logger
-	service := handlers.NewServer(logger, authRepo)
+	service := handlers.NewServer(logger, authRepo,acc,cc,ccava,resc)
 
 	protos.RegisterAuthServer(serverRegistar, service)
 	err = serverRegistar.Serve(lis)
